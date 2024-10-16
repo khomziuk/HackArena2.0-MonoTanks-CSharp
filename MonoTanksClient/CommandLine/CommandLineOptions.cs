aacadaecb42465a026692489fb9214df60ee42ff
Lokinado <hisboak@gmail.com>
2024-10-16 16:02:59 +0200

    Added command line arguments handling

---
+++ MonoTanksClient/CommandLine/CommandLineOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace MonoTanksClient.CommandLine;
+
+public class CommandLineOptions
+{
+    [Option('h', "host", Required = false, HelpText = "Set host.")]
+    public string Host { get; set; }
+
+    [Option('p', "port", Required = false, HelpText = "Set port.")]
+    public string Port { get; set; }
+
+    [Option('n', "nickname", Required = true, HelpText = "Set nickname.")]
+    public string Nickname { get; set; }
+
+    [Option('c', "code", Required = false, HelpText = "Set join code.")]
+    public string Code { get; set; }
+}
