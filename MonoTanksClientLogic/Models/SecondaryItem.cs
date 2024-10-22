b483fae04ac8c49771524b5546fa9d3c420544b0
Lokinado <hisboak@gmail.com>
2024-10-22 22:09:47 +0200

    Deleted models

--- MonoTanksClientLogic/Models/SecondaryItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MonoTanksClientLogic;
-
-/// <summary>
-/// Represents a secondary item on the map.
-/// </summary>
-/// <param name="X">The x coordinate of the item.</param>
-/// <param name="Y">The y coordinate of the item.</param>
-/// <param name="Type">The type of the item.</param>
-public record class SecondaryItem(int X, int Y, SecondaryItemType Type);
