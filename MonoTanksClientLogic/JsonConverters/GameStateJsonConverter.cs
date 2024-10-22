eb2df4635ca81238be35bffb4490fbd78fea6bb9
Lokinado <hisboak@gmail.com>
2024-10-22 22:42:51 +0200

    Implmented new up to spec json converters

---
+++ MonoTanksClientLogic/JsonConverters/GameStateJsonConverter.cs
@@ -0,0 +1,49 @@
+﻿using MonoTanksClientLogic.Networking;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MonoTanksClientLogic.JsonConverters;
+
+internal class GameStateJsonConverter : JsonConverter<GameState>
+{
+    public override GameState? ReadJson(JsonReader reader, Type objectType, GameState? existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        var jsonObject = JObject.Load(reader);
+
+        var id = jsonObject["id"]!.ToObject<string>()!;
+        var tick = jsonObject["tick"]!.ToObject<float>();
+
+        List<GamePlayer> players = new();
+        foreach (var player in (JArray)jsonObject["players"]!)
+        {
+            players.Add(player.ToObject<GamePlayer>()!);
+        }
+
+        var rawMap = (JArray)jsonObject["map"]!["tiles"]!;
+        int rows = rawMap.Count;
+        int cols = rawMap[0].Count();
+        var map = new Tile[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            var rowArray = (JArray)rawMap[i];
+
+            for (int j = 0; j < rowArray.Count; j++)
+            {
+                map[j, i] = rowArray[j].ToObject<Tile>()!;
+            }
+        }
+
+        List<Zone> zones = new();
+        foreach (var zone in (JArray)jsonObject["map"]!["zones"]!)
+        {
+            zones.Add(zone.ToObject<Zone>()!);
+        }
+
+        return new GameState(id, tick, players.ToArray(), map, zones.ToArray());
+    }
+
+    public override void WriteJson(JsonWriter writer, GameState? value, JsonSerializer serializer)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ MonoTanksClientLogic/JsonConverters/ZoneJsonConverter.cs
@@ -0,0 +1,37 @@
+﻿using MonoTanksClientLogic.Networking;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static MonoTanksClientLogic.Networking.ZoneStatus;
+
+namespace MonoTanksClientLogic.JsonConverters;
+
+public class ZoneJsonConverter : JsonConverter<Zone>
+{
+    public override Zone? ReadJson(JsonReader reader, Type objectType, Zone? existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        JObject jsonObject = JObject.Load(reader);
+
+        var x = jsonObject["x"]!.ToObject<long>()!;
+        var y = jsonObject["y"]!.ToObject<long>()!;
+        var width = jsonObject["width"]!.ToObject<long>()!;
+        var height = jsonObject["height"]!.ToObject<long>()!;
+        var index = jsonObject["index"]!.ToObject<int>()!;
+        var status = jsonObject["status"]!["type"]!.ToObject<string>()!;
+        ZoneStatus zoneStatus = status switch
+        {
+            "neutral" => new Neutral()!,
+            "beingCaptured" => jsonObject["status"]!.ToObject<BeingCaptured>()!,
+            "captured" => jsonObject["status"]!.ToObject<Captured>()!,
+            "beingContested" => jsonObject["status"]!.ToObject<BeingContested>()!,
+            "beingRetaken" => jsonObject["status"]!.ToObject<BeingRetaken>()!,
+            _ => throw new NotSupportedException()
+        };
+
+        return new Zone(index, x, y, width, height, zoneStatus);
+    }
+
+    public override void WriteJson(JsonWriter writer, Zone? value, JsonSerializer serializer)
+    {
+        throw new NotSupportedException();
+    }
+}
