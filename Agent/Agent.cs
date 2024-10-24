d36517e054378f037a9b419537dfd3d1e0bd604e
Lokinado <hisboak@gmail.com>
2024-10-24 03:37:07 +0200

    Added example agent implementation

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -1,91 +1,194 @@
 ﻿using MonoTanksClientLogic;
-using MonoTanksClientLogic.Networking;
+using MonoTanksClientLogic.Enums;
+using MonoTanksClientLogic.Models;
+using System.ComponentModel.DataAnnotations;
 
-namespace Agent
+namespace Agent;
+
+public class Agent : IAgent
 {
-    public class Agent : IAgent
+    private string myId;
+
+    public Agent(LobbyData lobbyData)
     {
-        public Agent(LobbyData lobbyData)
-        {
-            // Do your agent initialization here.
-        }
+        this.myId = lobbyData.PlayerId;
+    }
 
-        public AgentResponse NextMove(GameState gameState)
-        {
-            // Define behaviour of your bot in this method. 
-            // This method will be called every game tick with most recent game state.
-            // Use AgentResponse to return your action for each game tick.
+    public void OnSubsequentLobbyData(LobbyData lobbyData) { }
 
-            //Bot that randomly choses one of all possible agent responses.
-            var rand = new Random();
-            return rand.Next(0, 18) switch
+    public AgentResponse NextMove(GameState gameState)
+    {
+        Console.WriteLine("Map:");
+        for (int y = 0; y < gameState.Map.GetLength(0); y++)
+        {
+            for (int x = 0; x < gameState.Map.GetLength(1); x++)
             {
-                0 => AgentResponse.Pass(),
-                1 => AgentResponse.Move(MovementDirection.Backward),
-                2 => AgentResponse.Move(MovementDirection.Forward),
-                3 => AgentResponse.Rotate(null, null),
-                4 => AgentResponse.Rotate(null, Rotation.Left),
-                5 => AgentResponse.Rotate(null, Rotation.Right),
-                6 => AgentResponse.Rotate(Rotation.Left, null),
-                7 => AgentResponse.Rotate(Rotation.Left, Rotation.Left),
-                8 => AgentResponse.Rotate(Rotation.Left, Rotation.Right),
-                9 => AgentResponse.Rotate(Rotation.Right, null),
-                10 => AgentResponse.Rotate(Rotation.Right, null),
-                11 => AgentResponse.Rotate(Rotation.Right, Rotation.Left),
-                12 => AgentResponse.Rotate(Rotation.Right, Rotation.Right),
-                13 => AgentResponse.UseAbility(AbilityType.DropMine),
-                14 => AgentResponse.UseAbility(AbilityType.FireBullet),
-                15 => AgentResponse.UseAbility(AbilityType.FireDoubleBullet),
-                16 => AgentResponse.UseAbility(AbilityType.UseLaser),
-                17 => AgentResponse.UseAbility(AbilityType.UseRadar),
-                _ => throw new NotSupportedException(),
-            };
-        }
+                Tile tile = gameState.Map[y, x];
+                char symbol = ' ';
+                if (tile.IsVisible)
+                {
+                    symbol = '.';
+                }
 
-        public void OnGameEnd(GameEnd gameEnd)
-        {
-            // Define what your program should do when game is finished.
-        }
+                if (tile.ZoneIndex != null)
+                {
+                    int zoneIndex = (int)gameState.Map[y, x].ZoneIndex!;
+                    symbol = tile.IsVisible ? (char)zoneIndex : (char)(zoneIndex + 32);
+                }
 
-        public void OnGameStarting()
-        {
-            // Define what your program should do when game is starting.
-        }
-
-        public void OnSubsequentLobbyData(LobbyData lobbyData)
-        {
-            // Define what should happen when lobby is changed.
-            // For example when somebody new joins or disconnects
-            // new LobbyData is sent and this method is run.
-        }
-
-        public void OnWarningReceived(Warning warning, string? message)
-        {
-            // Define what your program should do when game is warning is recieved.
-
-            switch (warning)
-            {
-                case Warning.PlayerAlreadyMadeActionWarning:
+                foreach (var entity in gameState.Map[y, x].Entities)
+                {
+                    if (entity is Tile.Wall)
                     {
-                        Console.WriteLine("Player already made action warning");
-                        break;
+                        symbol = '#';
                     }
-                case Warning.SlowResponseWarning:
+                    else if (entity is Tile.Item item)
                     {
-                        Console.WriteLine("Slow response warning");
-                        break;
+                        symbol = item.ItemType switch
+                        {
+                            SecondaryItemType.Laser => 'L',
+                            SecondaryItemType.Mine => 'M',
+                            SecondaryItemType.Radar => 'R',
+                            SecondaryItemType.DoubleBullet => 'D',
+                            SecondaryItemType.Unknown => '?',
+                            _ => throw new NotSupportedException()
+                        };
                     }
-                case Warning.ActionIgnoredDueToDeadWarning:
+                    else if (entity is Tile.OwnTank ownTank)
                     {
-                        Console.WriteLine("Action ignored due to dead warning");
-                        break;
+                        symbol = ownTank.Direction switch
+                        {
+                            Direction.Down => 'V',
+                            Direction.Left => '<',
+                            Direction.Up => '^',
+                            Direction.Right => '>',
+                            _ => throw new NotSupportedException()
+                        };
+
+                        // There is also turret direction
+                        // ownTank.Turret.Direction
                     }
-                case Warning.CustomWarning:
+                    else if (entity is Tile.EnemyTank)
                     {
-                        Console.WriteLine($"Custom warning: {message ?? "no message"}");
-                        break;
+                        symbol = '#';
                     }
+                    else if (entity is Tile.Bullet bullet)
+                    {
+                        if (bullet.Type == BulletType.Basic)
+                        {
+                            symbol = bullet.Direction switch
+                            {
+                                Direction.Down => '↓',
+                                Direction.Left => '←',
+                                Direction.Up => '↑',
+                                Direction.Right => '→',
+                                _ => throw new NotSupportedException()
+                            };
+                        }
+                        else
+                        {
+                            symbol = bullet.Direction switch
+                            {
+                                Direction.Down => '⇊',
+                                Direction.Left => '⇇',
+                                Direction.Up => '⇈',
+                                Direction.Right => '⇉',
+                                _ => throw new NotSupportedException()
+                            };
+                        }
+                    }
+                    else if (entity is Tile.Laser laser)
+                    {
+                        symbol = laser.Orientation switch
+                        {
+                            LaserDirection.Horizontal => '-',
+                            LaserDirection.Vertical => '|',
+                            _ => throw new NotSupportedException()
+                        };
+                    }
+                    else if (entity is Tile.Mine mine)
+                    {
+                        symbol = 'X';
+                    }
+                }
+                Console.Write(symbol);
+                Console.Write(' ');
             }
+            Console.Write("\n");
+        }
+
+        //Bot that randomly choses one of all possible agent responses.
+        var rand = new Random();
+        return rand.Next(0, 18) switch
+        {
+            0 => AgentResponse.Pass(),
+            1 => AgentResponse.Move(MovementDirection.Backward),
+            2 => AgentResponse.Move(MovementDirection.Forward),
+            3 => AgentResponse.Rotate(null, null),
+            4 => AgentResponse.Rotate(null, Rotation.Left),
+            5 => AgentResponse.Rotate(null, Rotation.Right),
+            6 => AgentResponse.Rotate(Rotation.Left, null),
+            7 => AgentResponse.Rotate(Rotation.Left, Rotation.Left),
+            8 => AgentResponse.Rotate(Rotation.Left, Rotation.Right),
+            9 => AgentResponse.Rotate(Rotation.Right, null),
+            10 => AgentResponse.Rotate(Rotation.Right, null),
+            11 => AgentResponse.Rotate(Rotation.Right, Rotation.Left),
+            12 => AgentResponse.Rotate(Rotation.Right, Rotation.Right),
+            13 => AgentResponse.UseAbility(AbilityType.DropMine),
+            14 => AgentResponse.UseAbility(AbilityType.FireBullet),
+            15 => AgentResponse.UseAbility(AbilityType.FireDoubleBullet),
+            16 => AgentResponse.UseAbility(AbilityType.UseLaser),
+            17 => AgentResponse.UseAbility(AbilityType.UseRadar),
+            _ => throw new NotSupportedException(),
+        };
+    }
+
+    public void OnGameEnd(GameEnd gameEnd)
+    {
+        // Define what your program should do when game is finished.
+        GameEndPlayer winner = gameEnd.Players[0];
+        if (winner.Id == this.myId)
+        {
+            Console.WriteLine("I won!");
+        }
+
+        foreach (var player in gameEnd.Players)
+        {
+            Console.WriteLine($"{player.Nickname} - {player.Score}");
+        }
+    }
+
+    public void OnGameStarting()
+    {
+        // Define what your program should do when game is starting.
+    }
+
+    public void OnWarningReceived(Warning warning, string? message)
+    {
+        // Define what your program should do when game is warning is recieved.
+
+        switch (warning)
+        {
+            case Warning.PlayerAlreadyMadeActionWarning:
+                {
+                    Console.WriteLine("Player already made action warning");
+                    break;
+                }
+            case Warning.SlowResponseWarning:
+                {
+                    Console.WriteLine("Slow response warning");
+                    break;
+                }
+            case Warning.ActionIgnoredDueToDeadWarning:
+                {
+                    Console.WriteLine("Action ignored due to dead warning");
+                    break;
+                }
+            case Warning.CustomWarning:
+                {
+                    Console.WriteLine($"Custom warning: {message ?? "no message"}");
+                    break;
+                }
         }
     }
 }
